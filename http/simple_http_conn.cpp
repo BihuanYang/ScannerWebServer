18f0d83a85c47800eaba523562752d7f184fffd5
BihuanYang <51233408+BihuanYang@users.noreply.github.com>
2022-03-09 15:54:59 +0800

    Add files via upload

---
+++ http/simple_http_conn.cpp
@@ -0,0 +1,76 @@
+GET /favicon.ico HTTP/1.1
+Host: localhost:8080
+Connection: keep-alive
+sec-ch-ua: "Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"
+sec-ch-ua-mobile: ?0
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36
+sec-ch-ua-platform: "Linux"
+Accept: image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8
+Sec-Fetch-Site: same-origin
+Sec-Fetch-Mode: no-cors
+Sec-Fetch-Dest: image
+Referer: http://localhost:8080/
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9
+
+
+
+
+
+
+POST /5 HTTP/1.1
+Host: localhost:8080
+Connection: keep-alive
+Content-Length: 106884
+Cache-Control: max-age=0
+sec-ch-ua: "Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Linux"
+Upgrade-Insecure-Requests: 1
+Origin: http://localhost:8080
+Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryq6iZzFnAIXXEISZ8
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webapng,**;q=0.8,ap
+Sec-Fetch-Site: same-origin
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Referer: http://localhost:8080/
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9
+
+
+
+
+
+
+
+POST /5 HTTP/1.1
+Host: localhost:8080
+Connection: keep-alive
+Content-Length: 106884
+Cache-Control: max-age=0
+sec-ch-ua: "Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Linux"
+Upgrade-Insecure-Requests: 1
+Origin: http://localhost:8080
+Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryq6iZzFnAIXXEISZ8
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/a
+Sec-Fetch-Site: same-origin
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Referer: http://localhost:8080/
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9
+
+------WebKitFormBoundaryq6iZzFnAIXXEISZ8
+Content-Disposition: form-data; name="file"; filename="doc2.jpg"
+Content-Type: image/jpeg
+
+������R��B�9l�G����;}i���Z�5�F�2��g����p����Sƥ�)�]]~Oo'�5�M;��RKy#��a��ϧz�=?���Z�żqjZe����i��\β,�ΊAd`��V�y��03�q�[�$����V|�
+                                                                                                                                 /^��zq�������s�Yz�����?��8�����7�lnZ
+
+
